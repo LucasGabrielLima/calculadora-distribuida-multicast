11a6b95e1126d2a9324989cc7e21396a66cd9127
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2018-06-04 22:26:27 -0300

    separate socket for hearbeat thread etc

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import socket
 
+def send_to_group(message):
+	#The 'c' means it's a server message
+	sock.sendto(start_code + 'c' + message, (group_multicast, port))
+
 group_multicast = '224.1.1.1'
 port = 5007
-
-def send_to_group(message):
-	sock.sendto(start_byte+ 'c' + message, (group_multicast, port))
+start_code = '@'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
