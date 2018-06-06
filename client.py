41a96f9fa7515f6b8718801d5ab06dafc3fd04b4
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2018-06-06 15:46:16 -0300

    Revert "separate socket for hearbeat thread etc"
    This reverts commit 11a6b95e1126d2a9324989cc7e21396a66cd9127

--- client.py
+++ client.py
@@ -1,12 +1,10 @@
 import socket
 
-def send_to_group(message):
-	#The 'c' means it's a server message
-	sock.sendto(start_code + 'c' + message, (group_multicast, port))
-
 group_multicast = '224.1.1.1'
 port = 5007
-start_code = '@'
+
+def send_to_group(message):
+	sock.sendto(start_byte+ 'c' + message, (group_multicast, port))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
--- server.py
+++ server.py
@@ -5,8 +5,7 @@ import threading
 from time import sleep
 
 def send_to_group(message):
-	#The 's' means it's a server message, thus it's a hearbeat
-	sock.sendto(start_code + 's' + message, (group_multicast, port))
+	sock.sendto(start_byte+ 's' + message, (group_multicast, port))
 
 def heartbeat(sock):
 	print("Sending heartbeat...")
@@ -14,7 +13,7 @@ def heartbeat(sock):
 	while True:
 		try:
 			#Send Hearbeat to group
-			sock.sendto(start_byte + 's' + message, (group_multicast, port))
+			sock.sendto(start_byte+ 's' + message, (group_multicast, port))
 
 		except:
 			print("ERROR: Could not send heartbeat")
@@ -23,18 +22,15 @@ def heartbeat(sock):
 		sleep(1)
 
 
+		
+
 group_multicast = '224.1.1.1'
 port = 5007
-start_code = '@'
+start_byte = '@'
 
-# Create the hearbeat socket
-sockhb = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sockhb.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-
-#Cria socket for UDP Multicasting
+#Cria socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-#Seta TTL das mensagens
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
 #Seta timeout para 'listen' não bloquear execução
 sock.settimeout(0.2)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -44,8 +40,7 @@ req = struct.pack("4sl", socket.inet_aton(group_multicast), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, req)
 
 #Inicia thread que envia hearbeats a cada 10 segundos
-hb = threading.Thread(target=heartbeat, args=(sockhb,))
-#Faz thread encerrar execução quando o pai morrer
+hb = threading.Thread(target=heartbeat, args=(sock,))
 hb.daemon = True
 hb.start()
 
@@ -54,8 +49,5 @@ sleep(5)
 print('Ending heartbeat thread')
 
 sock.close()
-while True:
-	try:
-		print sock.recv(10240)
-	except:
-		pass
+#while True:
+#  print sock.recv(10240)
