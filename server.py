ccf4f4872c8cf15897c08157acfe20d3b1c8b345
Lucas Gabriel Lima <lgl15@inf.ufpr.br>
2018-06-06 18:37:53 -0300

    encerra graciosamente caso ctrl c etc

--- server.py
+++ server.py
@@ -7,18 +7,18 @@ import pickle #Biblioteca para serializar objetos de forma que possam ser enviad
 from time import sleep
 
 class Message(object):
-    sender = ''
-    sid = None #Server ID
-    payload = ''
+	sender = ''
+	sid = None #Server ID
+	payload = ''
 
-    def __init__(self, sender = 's', sid = None, payload = ''):
-    	self.sender = sender
-        self.sid = sid
-        self.payload = payload
+	def __init__(self, sender = 's', sid = None, payload = ''):
+		self.sender = sender
+		self.sid = sid
+		self.payload = payload
 
 
 def heartbeat(sock, sid):
-	print("Enviando heartbeat...")
+	print('Iniciando thread de heartbeats.')
 	message = Message('s', sid, 'heartbeat')
 	#Serializa objeto da mensagem
 	message = pickle.dumps(message)
@@ -44,7 +44,7 @@ def get_server_id():
 			message = pickle.loads(message)
 
 			#Adiciona servidores de heartbeats recebidos à lista
-			if(message.sender = 's'):
+			if(message.sender == 's'):
 				server_list[str(addr)] = [message.sid, datetime.datetime.now()]
 			if(message.sender == 's' and message.sid > greater_id):
 				greater_id = message.sid
@@ -55,21 +55,42 @@ def get_server_id():
 	#Se adiciona na lista de servidores
 	my_id = greater_id + 1
 	server_list[socket.gethostbyname(socket.gethostname())] = [my_id, datetime.datetime.now()]
-	print("Entrando no grupo de servidores. Meu ID é: " + my_id)
+	print("Entrando no grupo de servidores. Meu ID é: " + str(my_id))
 	return my_id
 
 #Chamada quando um heartbeat é recebido
 def update_server_list(addr):
 	now = datetime.datetime.now()
 	server_list[addr] = [server_list[addr][0], now]
-	print(now ': Hearbeat recebido de ' addr)
+	print(now + ': Hearbeat recebido de ' + addr)
+
 
 #Limpa servidores inativos da lista de servidores
 def clean_server_list():
 	for server in server_list:
-		if (datetime.datetime.now() - server_list[server][1] > datetime.timedelta(seconds = 10):
+		if (datetime.datetime.now() - server_list[server][1] > datetime.timedelta(seconds = 10)):
 			server_list.pop(server, None)
 			print('Server ' + server + ' se tornou inativo')
+
+def handle_req(req, addr):
+	pass
+
+def receive(data, address):
+	data = pickle.loads(data)
+
+	try:
+		if(data.sender == 'c'):
+			print("Requisição recebida do cliente " + address + ": " + data.payload)
+			clean_server_list()
+			handle_req(data, address)
+
+		if(data.sender == 's' and data.payload == 'heartbeat'):
+			update_server_list(address)
+			
+	except:
+		print("Você recebeu dados de fontes desconhecidas na porta de recebimento. Por favor mude a porta e tente novamente.")
+		sys.exit()
+
 
 
 group_multicast = '224.1.1.1'
@@ -80,6 +101,7 @@ sockhb = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ttl = struct.pack('b', 2)
 sockhb.settimeout(0.2)
 sockhb.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+print("Criado socket para envio de heartbeats.")
 
 #Cria socket UDP para comunicação
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -91,6 +113,7 @@ sock.bind((group_multicast, port))
 req = struct.pack("4sl", socket.inet_aton(group_multicast), socket.INADDR_ANY)
 
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, req)
+print("Criado socket para comunicação.")
 
 #Inicia lista (dicionário) de servidores
 server_list = {}
@@ -102,12 +125,25 @@ sid = get_server_id()
 hb = threading.Thread(target=heartbeat, args=(sockhb, sid,))
 hb.daemon = True
 hb.start()
-print('Iniciando thread de heartbeats')
+
+quit = False
+
+while True:
+	try:
+		data, address = sock.recv(10240)
+		receive(data, address)
+	except KeyboardInterrupt:
+		# Se receber um ctrl + c
+		break
+		
+	except:
+		pass
 
 
-sleep(5)
-print('Encerrando thread de heartbeats')
+print('Encerrando thread de heartbeats.')
 
 sock.close()
+sockhb.close()
+print("Encerrando sockets inicializados.")
 #while True:
 #  print sock.recv(10240)
